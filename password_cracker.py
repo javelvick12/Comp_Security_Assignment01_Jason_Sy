2e939eef1351beb18909db5a6dc8ea4663ce7589
Jason Velvick <jason.velvick@du.edu>
2025-09-19 13:00:20 -0600

    init python file

---
+++ password_cracker.py
@@ -0,0 +1,19 @@
+#Password Brute Force Cracker by Jason Velvick and Sy Pretto
+
+
+def sha256_handler():
+    pass
+
+def pbkdf2_handler():
+    pass
+
+def hash_reader():
+    pass
+
+
+def main():
+    hash_reader()
+
+
+if __name__ == "__main__":
+    main()
